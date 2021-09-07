6dfff7f178f51608e192ec1a4c794fab67d20cf6
voldomarich <vbekasov1987@gmail.com>
2021-09-07 23:55:17 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/tracker/pojo/Student.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker.pojo;
+
+import java.util.Date;
+
+public class Student {
+
+    private String name;
+    private String group;
+    private Date created;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public static class College {
+        public static void main(String[] args) {
+            Student student = new Student();
+            student.setName("Petr Petrovich Ivanov");
+            student.setCreated(new Date());
+            student.setGroup("103A");
+
+            System.out.println(student.getName() + " has been studying since " + student.getCreated() + " in the group № " + student.getGroup());
+        }
+    }
+}
