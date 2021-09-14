39b86b9d5026d508fe2d3a51a707efdd4fa4a476
voldomarich <vbekasov1987@gmail.com>
2021-09-14 23:59:25 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #201927]

---
+++ src/main/java/ru/job4j/tracker/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println(new Fact().calc(-1));
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Input should be number above zero.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
