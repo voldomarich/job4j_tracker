97dab8a08aa8e30584c0a38a029d28e88a338806
voldomarich <vbekasov1987@gmail.com>
2021-09-24 02:56:02 +0300

    1. Чтение из консоли. Класс Scanner. [#179514]

---
+++ src/main/java/ru/job4j/tracker/oop/Greeting.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker.oop;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите Ваш вопрос");
+        String name = input.nextLine();
+        int answer = new Random().nextInt(3);
+        if (answer == 0) {
+            System.out.println("Да");
+        }
+        if (answer == 1) {
+            System.out.println("Нет");
+        } else {
+            System.out.println("Может быть");
+        }
+        System.out.println(name + ", рад Вас видеть!");
+    }
+}
