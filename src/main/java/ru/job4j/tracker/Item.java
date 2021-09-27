c7f85c91f191535d79cffcfdb627188f23e8a6d1
voldomarich <vbekasov1987@gmail.com>
2021-09-27 20:00:10 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,19 +4,30 @@ public class Item {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
+    public Item() {
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Item(String name) {
         this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Чип для материнской платы" + name);
+        System.out.println("ID" + id);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item();
+        first.printInfo();
+        Item second = new Item("Чип для материнской платы");
+        second.printInfo();
+        Item third = new Item(20202021, "Чип для материнской платы");
+        third.printInfo();
     }
 }
