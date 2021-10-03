5351abf54814cfec0d2d3a4a45678ce502aec74b
voldomarich <vbekasov1987@gmail.com>
2021-10-04 00:39:32 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/tracker/pojo/Library.java
@@ -0,0 +1,36 @@
+package ru.job4j.tracker.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book ancientRome = new Book("Ancient Rome", 10);
+        Book poetry = new Book("Poetry", 11);
+        Book art = new Book("Art", 20);
+        Book cleanCode = new Book("Clean code", 21);
+        Book[] shelf = new Book[4];
+        shelf[0] = ancientRome;
+        shelf[1] = poetry;
+        shelf[2] = art;
+        shelf[3] = cleanCode;
+        for (int index = 0; index < shelf.length; index++) {
+            Book shel = shelf[index];
+            System.out.println(shel.getName() + " - " + shel.getCount());
+        }
+        System.out.println();
+        System.out.println("Replace Ancient Rome to Clean code");
+        Book temp = shelf[3];
+        shelf[3] = shelf[0];
+        shelf[0] = temp;
+        for (int index = 0; index < shelf.length; index++) {
+            Book shel = shelf[index];
+            System.out.println(shel.getName() + " - " + shel.getCount());
+        }
+        System.out.println();
+        System.out.println("Shown only clean code");
+        for (int index = 0; index < shelf.length; index++) {
+            Book shel = shelf[index];
+            if (shel.getName().equals("Clean code")) {
+                System.out.println(shel.getName()  + " - " + shel.getCount());
+            }
+        }
+    }
+}
