631e2b21c044c29d762264317abc0659ae3ad111
voldomarich <vbekasov1987@gmail.com>
2021-10-03 12:05:12 +0300

    5. Аннотация @Override [#174891]

---
+++ src/main/java/ru/job4j/tracker/inheritation/HTMLReport.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker.inheritation;
+
+public class HTMLReport extends TextReport {
+
+    @Override
+    public String generate(String name, String body) {
+        return "<h1>" + name + "</h1>" +
+                "<br/>" +
+                "<span>" + body + "</span>";
+    }
+}
--- src/main/java/ru/job4j/tracker/inheritation/JSONReport.java
+++ src/main/java/ru/job4j/tracker/inheritation/JSONReport.java
@@ -1,11 +1,13 @@
 package ru.job4j.tracker.inheritation;
 
 public class JSONReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
-        return {
-                "name" : "name",
-                System.lineSeparator(),
-                "body" : "body"
-                }
+        return  {
+            "name": "name",
+            System.lineSeparator(),
+            "body": "body"
+        }
     }
 }
