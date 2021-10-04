12624d11164f90947d23f8dd3f704780ec07e99a
voldomarich <vbekasov1987@gmail.com>
2021-10-04 20:54:33 +0300

    6. Date. Отображение даты. [#310017 #211733]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,6 +7,22 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public LocalDateTime getCreated() {
         return created;
     }
