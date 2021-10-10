74baca2f652901411681f6995030ea98a32f2dca
voldomarich <vbekasov1987@gmail.com>
2021-10-10 12:49:54 +0300

    3.1. Массив с пустыми ячейками [#176470 #213868]

--- src/test/java/ru/job4j/tracker/pojo/ShopDropTest.java
+++ src/test/java/ru/job4j/tracker/pojo/ShopDropTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker.pojo;
 
 import org.junit.Test;
-
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
@@ -10,33 +9,23 @@ public class ShopDropTest {
 
     @Test
     public void whenDropFirst() {
-        Book[] products = new Book[2];
-        products[0] = new Book("Peace and War", 10);
-        products[1] = new Book("Two for the Seesaw", 4);
-        Book[] rsl = ShopDrop.delete(products, 0);
-        assertThat(rsl[0].getName(), is("Two in the Seesaw"));
-        assertThat(rsl[1], is(nullValue()));
-    }
-
-    @Test
-    public void whenDropLast() {
-        Book[] products = new Book[2];
-        products[0] = new Book("Peace and War", 10);
-        products[1] = new Book("Two for the Seesaw", 4);
-        Book[] rsl = ShopDrop.delete(products, 1);
-        assertThat(rsl[0].getName(), is("Peace and War"));
+        Product[] products = new Product[2];
+        products[0] = new Product("Bread", 10);
+        products[1] = new Product("Egg", 4);
+        Product[] rsl = ShopDrop.delete(products, 1);
+        assertThat(rsl[0].getName(), is("Bread"));
         assertThat(rsl[1], is(nullValue()));
     }
 
     @Test
     public void whenArrayLengthIs4AndDeleteElementWithIndex1() {
-        Book[] products = new Book[4];
-        products[0] = new Book("Bread", 4);
-        products[1] = new Book("Egg", 10);
-        products[2] = new Book("Milk", 2);
-        products[3] = new Book("Fish", 3);
+        Product[] products = new Product[4];
+        products[0] = new Product("Bread", 4);
+        products[1] = new Product("Egg", 10);
+        products[2] = new Product("Milk", 2);
+        products[3] = new Product("Fish", 3);
         int index = 1;
-        Book[] rsl = ShopDrop.delete(products, index);
+        Product[] rsl = ShopDrop.delete(products, index);
         assertThat(rsl[index].getName(), is("Milk"));
         assertThat(rsl[2].getName(), is("Fish"));
         assertThat(rsl[products.length - 1], is(nullValue()));
@@ -44,14 +33,14 @@ public class ShopDropTest {
 
     @Test
     public void whenArrayLengthIs5AndDeleteElementWithIndex2() {
-        Book[] products = new Book[5];
-        products[0] = new Book("Bread", 4);
-        products[1] = new Book("Egg", 10);
-        products[2] = new Book("Milk", 2);
-        products[3] = new Book("Fish", 3);
-        products[4] = new Book("Fruit", 8);
+        Product[] products = new Product[5];
+        products[0] = new Product("Bread", 4);
+        products[1] = new Product("Egg", 10);
+        products[2] = new Product("Milk", 2);
+        products[3] = new Product("Fish", 3);
+        products[4] = new Product("Fruit", 8);
         int index = 2;
-        Book[] rsl = ShopDrop.delete(products, index);
+        Product[] rsl = ShopDrop.delete(products, index);
         assertThat(rsl[1].getName(), is("Egg"));
         assertThat(rsl[index].getName(), is("Fish"));
         assertThat(rsl[3].getName(), is("Fruit"));
