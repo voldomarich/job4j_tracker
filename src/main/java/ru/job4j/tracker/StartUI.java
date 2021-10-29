15e59a1f1cc3e4a2e13561c08a73fc498bf3c151
voldomarich <vbekasov1987@gmail.com>
2021-10-29 13:51:59 +0300

    2.4. Реализация класса StartUI. Вывод всех заявок [#500745]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -17,7 +17,17 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
-            } else if (select >= 0 && select < 6) {
+            } else if (select == 1) {
+                System.out.println("=== Show all items ===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println("Список заявок: " + item);
+                    }
+                } else {
+                    System.out.println("Хранилище ещё не содержит заявок");
+                }
+            } else if (select > 0 && select < 6) {
                 run = false;
             }
         }
