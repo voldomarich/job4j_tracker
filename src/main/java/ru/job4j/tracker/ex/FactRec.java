2d8f8c87143abed31e8717a19d49e018326f5581
voldomarich <vbekasov1987@gmail.com>
2021-11-11 10:10:06 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #227001]

--- src/main/java/ru/job4j/tracker/ex/FactRec.java
+++ src/main/java/ru/job4j/tracker/ex/FactRec.java
@@ -2,11 +2,11 @@ package ru.job4j.tracker.ex;
 
 public class FactRec {
     public static int calc(int n) {
-        if (n <= 1) {
+        if (n == 0 || n == 1) {
             return 1;
         }
         return calc(n - 1) * n;
-        }
+    }
 
     public static void main(String[] args) {
         int rsl = calc(11);
