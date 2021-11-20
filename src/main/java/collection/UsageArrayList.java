6f8579769ba5c2c0e30abe49b78e985abc8b97b5
voldomarich <vbekasov1987@gmail.com>
2021-11-20 13:06:34 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094 #230513]

--- src/main/java/collection/UsageArrayList.java
+++ src/main/java/collection/UsageArrayList.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class UsageArrayList {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>();
-        names.add = "Petr";
-        names.add = "Ivan";
-        names.add = "Stepan";
-        for(Object value : names) {
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
+        for (Object value : names) {
             System.out.println(value);
         }
     }
