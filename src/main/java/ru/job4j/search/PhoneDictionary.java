ab71aa2cf0a5b0dcd4024db12867fdf66505c7d2
voldomarich <vbekasov1987@gmail.com>
2021-11-22 16:49:12 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,19 @@
+package ru.job4j.search;
+
+public class PhoneDictionary {
+
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        if  (key.contains() > 2 && key.contains().getName || key.contains().getSurname
+        || key.contains().persons.getPhone || key.contains().getAddress) {
+            return persons;
+        }
+        ArrayList<Person> result = new ArrayList<>();
+        return result;
+    }
+}
