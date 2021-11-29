794af3e02e442e5f450743d7ec91cbbea748fe60
voldomarich <vbekasov1987@gmail.com>
2021-11-29 19:18:07 +0300

    6. Изменить программу Tracker из 2-го модуля [#10039 #233916]

--- src/main/java/ru/job4j/tracker/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/tracker/SingleTracker.java
@@ -30,7 +30,7 @@ public final class SingleTracker {
         return tracker.findAll();
     }
 
-    public Item[] findByName(String key) {
+    public List<Item> findByName(String key) {
         return tracker.findByName(key);
     }
 
