3ecdb8c50387b0702a0a44d2e1024a52afa8f226
voldomarich <vbekasov1987@gmail.com>
2022-01-06 18:03:16 +0300

    3. Ссылки на методы [#70608 #247618]

--- src/main/java/ru/job4j/lambda/RefMethod.java
+++ src/main/java/ru/job4j/lambda/RefMethod.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class RefMethod {
+
     public static void main(String[] args) {
         List<String> names = Arrays.asList(
                 "Ivan",
