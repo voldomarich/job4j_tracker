c507215ec655b11db24457a2afbc5e7b97fe5043
voldomarich <vbekasov1987@gmail.com>
2022-01-06 18:17:07 +0300

    3. Ссылки на методы [#70608 #247618]

--- src/test/java/ru/job4j/collection/DempDescCompTest.java
+++ src/test/java/ru/job4j/collection/DempDescCompTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 
-
 public class DempDescCompTest {
     @Test
     public void compare() {
