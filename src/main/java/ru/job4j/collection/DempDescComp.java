b7646551d4bd10947c78816638005b24d25624c5
voldomarich <vbekasov1987@gmail.com>
2022-01-08 02:24:49 +0300

    2. Отсортировать департаменты [#34352 #247438]

--- src/main/java/ru/job4j/collection/DempDescComp.java
+++ src/main/java/ru/job4j/collection/DempDescComp.java
@@ -3,6 +3,7 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class DempDescComp implements Comparator<String> {
+
     @Override
     public int compare(String o1, String o2) {
         String[] a = o1.split("/");
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -1,26 +1,25 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Departments {
     public static List<String> fillGaps(List<String> deps) {
         Set<String> tmp = new LinkedHashSet<>();
         for (String value : deps) {
+            String start = "";
             for (String el : value.split("/")) {
-                tmp.add(el);
+                start += "/" + el;
+                tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);
     }
 
-    public static void sortAsc(List<String> orgs) {
-
+    public static void sortAsc(List<String> deps) {
+        Collections.sort(deps, Comparator.naturalOrder());
     }
 
-    public static void sortDesc(List<String> orgs) {
-
+    public static void sortDesc(List<String> deps) {
+        Collections.sort(deps, new DempDescComp());
     }
 }
