10d802f20f7e1d719ca0acf5194d705753fe3098
voldomarich <vbekasov1987@gmail.com>
2022-01-08 11:24:50 +0300

    2. Отсортировать департаменты [#34352 #247438]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -8,7 +8,7 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                start += "/" + el;
+                start += "".equals(start) ? el : "/" + el;
                 tmp.add(start);
             }
         }
