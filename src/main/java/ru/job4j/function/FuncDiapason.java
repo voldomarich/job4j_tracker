a475927e6b3187cf02d9dff01e3f8e6e9d9b1d99
voldomarich <vbekasov1987@gmail.com>
2022-01-09 15:34:03 +0300

    5. Подсчет функции в диапазоне. [#24251]

--- src/main/java/ru/job4j/function/FuncDiapason.java
+++ src/main/java/ru/job4j/function/FuncDiapason.java
@@ -1,4 +1,4 @@
-package ru.job4j.lambda;
+package ru.job4j.function;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/ru/job4j/function/FuncDiapasonTest.java
+++ src/test/java/ru/job4j/function/FuncDiapasonTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.function;
 
 import org.junit.Test;
-import ru.job4j.lambda.FuncDiapason;
 
 import java.util.Arrays;
 import java.util.List;
