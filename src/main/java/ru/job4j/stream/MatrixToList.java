1e8ec0b8e9ca12e9ccd271ab556535b39af2d1ae
voldomarich <vbekasov1987@gmail.com>
2022-01-12 19:58:54 +0300

    5. Преобразование матрицы чисел в список чисел. [#110228 #249993]

--- src/main/java/ru/job4j/stream/MatrixToList.java
+++ src/main/java/ru/job4j/stream/MatrixToList.java
@@ -1,13 +1,24 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MatrixToList {
     public static List<Integer> convert(Integer[][] matrix) {
-            Stream.of(matrix).flatMap().collect(Collectors.toList());
-            return new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        for (Integer[] integers : matrix) {
+            for (Integer i : integers) {
+                Stream.of(matrix)
+                        .flatMap(integer -> Stream.of(integers))
+                        .map(integer -> i)
+                        .collect(Collectors.toList());
+            }
         }
+        return list;
     }
+}
+
+
