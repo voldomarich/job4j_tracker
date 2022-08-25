8cd2f87a1d35300a372d889d9f9067fe4a44711d
voldomarich <vbekasov1987@gmail.com>
2022-08-25 04:07:54 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,57 @@
+package ru.job4j.early;
+
+public class PasswordValidator {
+
+    public static String validate(String password) {
+
+        if (password == null) {
+            throw new IllegalArgumentException("Строка пуста");
+        }
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException(
+                    String.format("Длина пароля: %s должна быть от 8 до 32 символов включительно", password));
+        }
+        for (int i = 0; i < password.length(); i++) {
+            if (isWhiteSpace(password.charAt(i))) {
+                throw new IllegalArgumentException(
+                        String.format("Пароль: %s не должен содержать пробел", password));
+            }
+            if (!Character.isLowerCase(password.charAt(i))) {
+                throw new IllegalArgumentException(
+                        String.format("Пароль: %s должен содержать хотя бы один символ в нижнем регистре", password));
+            }
+            if (!Character.isUpperCase(password.charAt(i))) {
+                throw new IllegalArgumentException(
+                        String.format("Пароль: %s должен содержать хотя бы один символ в верхнем регистре", password));
+            }
+            if (!Character.isDigit(password.charAt(i))) {
+                throw new IllegalArgumentException(
+                        String.format("Пароль: %s должен содержать хотя бы одну цифру", password));
+            }
+            if (!isSpecialSymbol(password.charAt(i))) {
+                throw new IllegalArgumentException(
+                        String.format("Пароль: %s должен содержать хотя бы один специальный символ", password));
+            }
+        }
+            String[] string = {"qwerty", "12345", "password", "admin", "user"};
+            for (String s : string) {
+                for (int a = 0; a < s.length(); a++) {
+                    if (password.contains(s)
+                            && (Character.isUpperCase(s.charAt(a))
+                            || Character.isLowerCase(s.charAt(a)))) {
+                        throw new IllegalArgumentException(
+                                String.format("Пароль: %s не должен содержать данное слово в любом регистре: %s", password, s));
+                    }
+                }
+            }
+        return password;
+    }
+
+    private static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 37 || code == 95;
+    }
+
+    private static boolean isWhiteSpace(int code) {
+        return code == 32;
+    }
+}
---
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -0,0 +1,93 @@
+package ru.job4j.early;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @Test
+    void checkStringEmpty() {
+        assertThatThrownBy(() -> PasswordValidator.validate(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Строка пуста");
+    }
+
+    @Test
+    void checkStringEmpty2() {
+        assertThatThrownBy(() -> PasswordValidator.validate(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .message()
+                .isNotEmpty();
+    }
+
+    @Test
+    void containsWhiteSpace() {
+        assertThatThrownBy(() -> PasswordValidator.validate("Jdnb FNFb820j%Jj%g"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("не должен содержать пробел");
+    }
+
+    @Test
+    void containsLessSymbols() {
+        assertThatThrownBy(() -> PasswordValidator.validate("swert"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("должна быть от 8 до 32 символов включительно");
+    }
+
+    @Test
+    void containsMoreSymbols() {
+        assertThatThrownBy(() -> PasswordValidator.validate("ChnAjDdv120vm_dsfsdcdasnuyjtfhdlfVNtreh"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("должна быть от 8 до 32 символов включительно");
+    }
+
+    @Test
+    void lower() {
+        assertThatThrownBy(() -> PasswordValidator.validate("LOOYRVHHG$10VF"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("хотя бы один символ в нижнем регистре");
+    }
+
+    @Test
+    void upper() {
+        assertThatThrownBy(() -> PasswordValidator.validate("lfkds$ivndfv"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("хотя бы один символ в верхнем регистре");
+    }
+
+    @Test
+    void digit() {
+        assertThatThrownBy(() -> PasswordValidator.validate("lfkd$siVn_dfv"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("хотя бы одну цифру");
+    }
+
+    @Test
+    void qwertyLike() {
+        assertThatThrownBy(() -> PasswordValidator.validate("PassWord123%"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("не должен содержать данное слово в любом регистре: password");
+    }
+
+    @Test
+    void qwertyLike2() {
+        assertThatThrownBy(() -> PasswordValidator.validate("Parol_12345"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("не должен содержать данное слово в любом регистре: 12345");
+    }
+
+    @Test
+    void check() {
+        String i = PasswordValidator.validate("Jhwsay11hyew%jNcb");
+        String expected = "Jhwsay11hyew%jNcb";
+        assertThat(i).isEqualTo(expected);
+    }
+
+    @Test
+    void check2() {
+        String i = PasswordValidator.validate("abcDgprjg100_sd");
+        String expected = "abcDgprjg100_sd";
+        assertThat(i).isEqualTo(expected);
+    }
+}
