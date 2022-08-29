eea4bb718dbb1e5f8ee608d53a5857f8f190779f
voldomarich <vbekasov1987@gmail.com>
2022-08-29 23:47:46 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #336165]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -4,7 +4,7 @@ public class PasswordValidator {
 
     public static String validate(String password) {
 
-        if (password == null) {
+        if (password.length() == 0) {
             throw new IllegalArgumentException("Строка пуста");
         }
         if (password.length() < 8 || password.length() > 32) {
@@ -13,47 +13,50 @@ public class PasswordValidator {
                             password));
         }
         for (int i = 0; i < password.length(); i++) {
-            if (isWhiteSpace(password.charAt(i))) {
-                throw new IllegalArgumentException(
-                        String.format("Пароль: %s не должен содержать пробел", password));
+            if (!isWhiteSpace(password.charAt(i))) {
+                return password;
             }
-            if (!Character.isLowerCase(password.charAt(i))) {
+            throw new IllegalArgumentException(
+                    String.format("Пароль: %s не должен содержать пробел", password));
+            if (Character.isLowerCase(password.charAt(i))) {
+                return password;
+            }
                 throw new IllegalArgumentException(
                         String.format("Пароль: %s должен содержать хотя бы один символ "
                                 + "в нижнем регистре", password));
+            if (Character.isUpperCase(password.charAt(i))) {
+                return password;
             }
-            if (!Character.isUpperCase(password.charAt(i))) {
-                throw new IllegalArgumentException(
-                        String.format("Пароль: %s должен содержать хотя бы один символ "
-                                + "в верхнем регистре", password));
+            throw new IllegalArgumentException(
+                    String.format("Пароль: %s должен содержать хотя бы один символ "
+                            + "в верхнем регистре", password));
+            if (Character.isDigit(password.charAt(i))) {
+                return password;
             }
-            if (!Character.isDigit(password.charAt(i))) {
-                throw new IllegalArgumentException(
-                        String.format("Пароль: %s должен содержать хотя бы одну цифру", password));
+            throw new IllegalArgumentException(
+                    String.format("Пароль: %s должен содержать хотя бы одну цифру", password));
+            if (isSpecialSymbol(password.charAt(i))) {
+                return password;
             }
-            if (!isSpecialSymbol(password.charAt(i))) {
-                throw new IllegalArgumentException(
-                        String.format("Пароль: %s должен содержать хотя бы один специальный символ",
-                                password));
-            }
+            throw new IllegalArgumentException(
+                    String.format("Пароль: %s должен содержать хотя бы один специальный символ",
+                            password));
         }
+
             String[] string = {"qwerty", "12345", "password", "admin", "user"};
             for (String s : string) {
-                for (int a = 0; a < s.length(); a++) {
-                    if (password.contains(s)
-                            && (Character.isUpperCase(s.charAt(a))
-                            || Character.isLowerCase(s.charAt(a)))) {
+                    if (password.contains(s.toLowerCase())
+                    || password.contains(s.toUpperCase())) {
                         throw new IllegalArgumentException(
                                 String.format("Пароль: %s не должен содержать данное слово "
                                         + "в любом регистре: %s", password, s));
                     }
                 }
-            }
         return password;
     }
 
     private static boolean isSpecialSymbol(int code) {
-        return code == 36 || code == 37 || code == 95 || code == 41 || code == 43;
+        return code == 36 || code == 37 || code == 91 || code == 93 || code == 95;
     }
 
     private static boolean isWhiteSpace(int code) {
