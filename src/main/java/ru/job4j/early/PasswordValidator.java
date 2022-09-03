331cc7c8596226605b13c193aff7b7807ca9f229
voldomarich <vbekasov1987@gmail.com>
2022-09-04 01:03:13 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #336165]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -12,41 +12,56 @@ public class PasswordValidator {
                     String.format("Длина пароля: %s должна быть от 8 до 32 символов включительно",
                             password));
         }
+        String[] string = {"qwerty", "12345", "password", "admin", "user"};
+        for (String s : string) {
+            if (password.contains(s.toLowerCase())
+                    || password.contains(s.toUpperCase())) {
+                throw new IllegalArgumentException(
+                        String.format("Пароль: %s не должен содержать данное слово "
+                                + "в любом регистре: %s", password, s));
+            }
+        }
         for (int i = 0; i < password.length(); i++) {
+            boolean statusWhiteSpace = false;
+            boolean statusLowerCase = false;
+            boolean statusUpperCase = false;
+            boolean statusDigit = false;
+            boolean statusSpecialSymbol = false;
 
             if (!isWhiteSpace(password.charAt(i))) {
+                statusWhiteSpace = true;
+            } else {
                 throw new IllegalArgumentException(
                         String.format("Пароль: %s не должен содержать пробел", password));
             }
             if (Character.isLowerCase(password.charAt(i))) {
+                statusLowerCase = true;
+            } else {
                 throw new IllegalArgumentException(
                         String.format("Пароль: %s должен содержать хотя бы один символ "
                                 + "в нижнем регистре", password));
             }
             if (Character.isUpperCase(password.charAt(i))) {
+                statusUpperCase = true;
+            } else {
                 throw new IllegalArgumentException(
                         String.format("Пароль: %s должен содержать хотя бы один символ "
                                 + "в верхнем регистре", password));
             }
             if (Character.isDigit(password.charAt(i))) {
+                statusDigit = true;
+            } else {
                 throw new IllegalArgumentException(
                         String.format("Пароль: %s должен содержать хотя бы одну цифру", password));
             }
             if (!isSpecialSymbol(password.charAt(i))) {
+                statusSpecialSymbol = true;
+            } else {
                 throw new IllegalArgumentException(
                         String.format("Пароль: %s должен содержать хотя бы один специальный символ",
                                 password));
             }
         }
-            String[] string = {"qwerty", "12345", "password", "admin", "user"};
-            for (String s : string) {
-                    if (password.contains(s.toLowerCase())
-                    || password.contains(s.toUpperCase())) {
-                        throw new IllegalArgumentException(
-                                String.format("Пароль: %s не должен содержать данное слово "
-                                        + "в любом регистре: %s", password, s));
-                    }
-                }
         return password;
     }
 
