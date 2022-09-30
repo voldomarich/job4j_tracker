e56e2a5a58458f7096ddc7ad974cafaf5054f7a3
voldomarich <vbekasov1987@gmail.com>
2022-09-30 11:15:59 +0300

    1. Liquibase. Установка. [#504892 #347284]

--- src/main/java/ru/job4j/tracker/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/tracker/Tracker.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker.tracker;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Tracker {
     private final List<Item> items = new ArrayList<>();
