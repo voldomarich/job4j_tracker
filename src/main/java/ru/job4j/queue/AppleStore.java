4079650a06e8947532026bf7968ac47506cad54a
voldomarich <vbekasov1987@gmail.com>
2023-10-27 23:29:57 +0300

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,29 @@
+package ru.job4j.queue;
+
+import java.util.Objects;
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        for (int i = 1; i < count; i++) {
+            queue.poll();
+        }
+        return Objects.requireNonNull(queue.poll()).name();
+    }
+
+    public String getFirstUpsetCustomer() {
+        for (int i = 0; i < count; i++) {
+            queue.poll();
+        }
+        return Objects.requireNonNull(queue.poll()).name();
+    }
+}
