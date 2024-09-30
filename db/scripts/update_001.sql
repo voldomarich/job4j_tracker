aea3c576857c006acfac9cfa525bdbbe8b5908b8
voldomarich <vbekasov1987@gmail.com>
2024-09-30 20:43:22 +0300

    1. Конфигурация Hibernate [#6872 #515112]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,5 +1,5 @@
 create table if not exists items (
-   id serial primary key,
-   name text,
-   created timestamp
+    id serial primary key,
+    name text,
+    created timestamp
 );
--- src/main/java/ru/job4j/tracker/HibernateRun.java
+++ src/main/java/ru/job4j/tracker/HibernateRun.java
@@ -9,7 +9,7 @@ import ru.job4j.tracker.tracker.Item;
 
 import java.util.List;
 
-public class HibernateRun {
+public class HibernateRun {яяя
     public static void main(String[] args) {
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure().build();
@@ -66,7 +66,7 @@ public class HibernateRun {
     public static List<Item> findAll(SessionFactory sf) {
         Session session = sf.openSession();
         session.beginTransaction();
-        List<Item> result = session.createQuery("from ru.job4j.tracker.Item", Item.class).list();
+        List<Item> result = session.createQuery("from Item", Item.class).list();
         session.getTransaction().commit();
         session.close();
         return result;
